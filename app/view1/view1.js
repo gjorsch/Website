51604d90d13788bda1bd1460348bafeffb4450b1
gjorsch <g.braeunig@gmail.com>
2015-11-22 19:19:25 +0100

    initial commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,20 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', ["$scope", "$http", function($scope, $http) {
+  $http.get("data/skos_groups.json").success(function(data){
+    var dispData = [];
+    var lessRespData;
+    data = data["@graph"];
+    for (var i = 0; i < data.length; i++) {
+		  lessRespData = data[i]["skosxl:literalForm"];
+			if (lessRespData != undefined){
+				if (lessRespData["@language"]== "de"){
+					dispData.push({id: data[i]["@id"],language: lessRespData["@language"], value: lessRespData["@value"]});
+				}
+			}
+  	}
+    $scope.data = dispData;
+    console.log($scope.data);
+  });
 }]);

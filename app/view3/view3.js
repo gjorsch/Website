a05c791e1826f5b56893d0bb907afc12b969041f
gjorsch <g.braeunig@gmail.com>
2015-11-23 21:57:04 +0100

    fixed hierachy 2lvl

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -13,40 +13,54 @@ angular.module('myApp.view3', ['ngRoute'])
   $http.get("data/skos_subjects.json").success(function(data){
     data = data["@graph"];
     //dispData: data
-    var dispData = [];
-    var dispDataNarrow = [];
+    var dispData = {};
+
     //find parent node
-    for (var i =0; i < data.length;i++){
+    for (var i=0; i<data.length; i++){
       if (!data[i]["broader"] && data[i]["narrower"]){
         console.log(data[i]);
         if (data[i]["prefLabel"] != undefined){
           for (var j =0; j < data[i]["prefLabel"].length;j++){
             //if possible use REGEX here for URI language selection
             if(data[i]["prefLabel"][j].slice(35,37)==="en"){
-              dispData.push({id:data[i]["@id"], label:data[i]["prefLabel"][j]});
+              dispData.parent = ({id:data[i]["@id"], label:data[i]["prefLabel"][j], narrower: []});
+              for (var k=0; k<data[i]["narrower"].length;k++){
+                dispData["parent"]["narrower"].push({id: data[i]["narrower"][k]});
+              };
             };
           };
         };
       };
     };
-    //find next level hierachy item
-    console.log(dispData[0]["id"]);
-    for (var i=0; i <data.length;i++){
-        if (data[i]["broader"]===dispData[0]["id"]){
-          //console.log(data[i]);
-          //console.log("hit!");
+
+    //define 2nd level hierachy items
+    for (var i=0; i<data.length; i++){
+      for (var j=0; j<dispData["parent"]["narrower"].length; j++){
+
+        if (data[i]["@id"]==dispData["parent"]["narrower"][j]["id"]){
           if (data[i]["prefLabel"] != undefined){
-            for (var j =0; j < data[i]["prefLabel"].length;j++){
-              //if possible use REGEX here for URI language selection
-              if(data[i]["prefLabel"][j].slice(35,37)==="en"){
-                dispDataNarrow.push({id:data[i]["@id"], label:data[i]["prefLabel"][j]});
-                console.log(data[i]["prefLabel"][j]);
+            for (var k =0; k<data[i]["prefLabel"].length;k++){
+              if(data[i]["prefLabel"][k].slice(35,37)==="en"){
+                dispData["parent"]["narrower"][j]["label"]= data[i]["prefLabel"][k];
+                if (data[i]["narrower"] != undefined){
+                  console.log("click!");
+                  dispData["parent"]["narrower"][j]["narrower"]=[];
+                  for (var l=0; l<data[i]["narrower"].length;l++){
+
+                    dispData["parent"]["narrower"][j]["narrower"].push({id: data[i]["narrower"][l]});
+                    console.log(data[i]["narrower"][l]);
+                    console.log(dispData["parent"]["narrower"][j]["narrower"]);
+                  };
+                };
               };
             };
           };
         };
+      };
     };
-    console.log(dispDataNarrow);
+
+    //find 3rd level hierachy items
+    console.log(dispData);
     $scope.data = dispData;
     console.log($scope.data);
   });

e9d1d168e0163cb997bfb6a98bcf14963e4e7073
gjorsch <g.braeunig@gmail.com>
2016-01-05 00:57:20 +0100

    partial collapsability

--- app/view4/view4.js
+++ app/view4/view4.js
@@ -32,4 +32,24 @@ angular.module('myApp.view4', ['ngRoute'])
     highlightKeyword(selection);
 
   };
-}]);
+}])
+.directive("collapseDirective", function() {
+    return {
+        restrict: "A",
+        link: function(scope,elem, attrs) {
+          $(elem).click(function (e) {
+              var children = $(elem).next('li.parent_li').find(' > ul > li');
+              //var children = $(elem);
+              console.log(children);
+              if (children.is(":visible")) {
+                  children.hide('fast');
+                  $(elem).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+              } else {
+                  children.show('fast');
+                  $(elem).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+              }
+              e.stopPropagation();
+          });
+        }
+    }
+});
